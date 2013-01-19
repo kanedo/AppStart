27aa0d5d0829ec7a3de597c4d768af096b66f361
Gabriel Bretschner <info@kanedo.net>
2013-01-19 10:19:25 +0100

    added some commandline options

--- server/app-start.php
+++ server/app-start.php
@@ -5,9 +5,17 @@
 	use Kanedo\AppStartServer;
 	use Ratchet\WebSocket\WsServer;
 	
+	$options = getopt("vh", array("verbose", "help"));
+	$verbose = (array_key_exists("v", $options) || array_key_exists("verbose", $options))? true : false;
+	if(array_key_exists("h", $options) || array_key_exists("help", $options)){
+		echo "AppStart Server usage: \n";
+		echo "-v --verbose to enable logging \n";
+		echo "-h --help to see this message \n";
+		exit(0);
+	}
 		$server = IoServer::factory(
 		        new WsServer(
-		            new AppStartServer()
+		            new AppStartServer($verbose)
 		        )
 		      , 8080
 		    );
