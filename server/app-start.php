ea0e7a5f74de090d22311e8481faa49e4db1cb7b
Gabriel Bretschner <info@kanedo.net>
2013-01-19 10:28:39 +0100

    catch bindig errors

--- server/app-start.php
+++ server/app-start.php
@@ -8,7 +8,7 @@
 	$options = getopt("vhp:", array("verbose", "help", "port:"));
 	if(array_key_exists("h", $options) || array_key_exists("help", $options)){
 		echo "AppStart Server usage: \n";
-		echo "-p --port configure the port on which the server listens\n";
+		echo "-p --port configure the port on which the server listens (Default 8080)\n";
 		echo "-v --verbose to enable logging \n";
 		echo "-h --help to see this message \n";
 		exit(0);
@@ -16,11 +16,19 @@
 	$verbose = (array_key_exists("v", $options) || array_key_exists("verbose", $options))? true : false;
 	$port = (array_key_exists("p", $options))? (int)$options['p'] : 8080;
 	$port = (array_key_exists("port", $options))? (int)$options['port'] : $port;
+	
+	/**
+	 * Run baby!
+	 **/
+	try{
 		$server = IoServer::factory(
-		        new WsServer(
-		            new AppStartServer($verbose)
-		        )
-		      , $port
-		    );
-
-	    $server->run();
+	        new WsServer(
+	            new AppStartServer($verbose)
+	        )
+	      , $port
+	    );
+	
+    	$server->run();
+	}catch(React\Socket\ConnectionException $e){
+		echo "{$e->getMessage()}\n";
+	}
