d98ccb693d95891de3e3ecc0941a41df54bc955b
Gabriel Bretschner <software@kanedo.net>
2013-03-25 09:36:52 +0100

    added basic Server script with install-description

--- interface.sh
+++ interface.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 PORT=1822
-
+HOST=$(hostname -f)
+echo "AppStart server started"
+echo "You can now reach the interface at $(tput bold)${HOST}:${PORT}$(tput sgr0)"
+echo "To Stop serving press CTRL + C"
+echo ""
+echo "If you're using an iOS device, open the URL and hit: Add to Homescreen"
+echo "Open the new 'App' and wait a few seconds. You can now stop this server."
 PHP -S 0.0.0.0:$PORT -t PATH/public/
